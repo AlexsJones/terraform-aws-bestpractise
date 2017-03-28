01f32879ef4a9906153e25c08ad2fe195f67e110
AlexsJones <tibbar@freedommail.ch>
2017-03-28 09:22:58 +0100

    adding some ingress rules

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # terraform-aws-bestpractise
 
-This attempts to emulate theAWS best practise guide for isolation of availability zones
-
+This attempts to emulate the AWS best practise guide for isolation of availability zones
+(Terraforming probably isn't best practise but its a WIP)
 To use this you'll need to generate `keys/ami_keys (private) and keys/ami_keys.pub`
 
 
--- security-groups.tf
+++ security-groups.tf
@@ -83,4 +83,18 @@ resource "aws_security_group" "allow_bastion" {
 
     cidr_blocks = ["${var.vpc_cidr_block}"]
   }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["${var.public_cidr_block1}", "${var.public_cidr_block2}"]
+  }
+
+  egress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["${var.public_cidr_block1}", "${var.public_cidr_block2}"]
+  }
 }
